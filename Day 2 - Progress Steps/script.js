2dbfe1d2dc52fd7caf064054f7a39fce788331c0
cassirambd <cassiram15@gmail.com>
2022-11-28 23:57:21 -0300

    function update

--- Day 2 - Progress Steps/script.js
+++ Day 2 - Progress Steps/script.js
@@ -11,4 +11,26 @@ next.addEventListener('click', () => {
   if (currentActive > circles.length) {
     currentActive = circles.length;
   }
+
+  update();
 });
+
+prev.addEventListener('click', () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update();
+});
+
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
+}
